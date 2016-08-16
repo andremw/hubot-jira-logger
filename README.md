b84b7f8809071d011faa46215b518d5bf166da6b
Rodrigo Carvalho <rnascimentodecarva@unomaha.edu>
2016-08-16 13:35:43 +0000

    Initial commit with Jira Logger initial code.

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# jira-logger
+# hubot-jira-logger
+
+Hubot script for logging hours on Jira.
+
+See [`src/jira-logger.coffee`](src/jira-logger.coffee) for full documentation.
+
+## Installation
+
+In hubot project repo, run:
+
+`npm install hubot-jira-logger --save`
+
+Then add **hubot-jira-logger** to your `external-scripts.json`:
+
+```json
+[
+  "hubot-jira-logger"
+]
+```
+
+## Sample Interaction
+
+Privately, user1 sends a message to hubot.
+```
+user1>> hubot add-user <user1.email> password <password>
+hubot>> Your user was successfully added to my database.
+```
+
+If another user2 tries to add in credentials for other people.
+```
+user2>> hubot add-user <user1.email> password <password>
+hubot>> Stop trying to use other people\'s usernames, bro!
+```
+
+Logging hours on JIRA.
+```
+user1>> hubot log <hours> on <task>
+hubot>> Yay! You logged <hours> on <task>
+```
+
+## Checklist
+- [x] Find a way to add users to hubot
+- [x] Create command to log hours
+- [ ] Add possibility of including comments to hour logs
+- [ ] Create command to move task status
+- [ ] Create command to assign task to other people
+- [ ] Create command to comment on task directly
+
+## NPM Module
+
+https://www.npmjs.com/package/hubot-jira-logger
---
+++ src/jira-logger.js
@@ -0,0 +1,103 @@
+// Description
+//   Hubot script for logging hours on Jira.
+//
+// Configuration:
+//   JIRA_PROJECT_TOKEN - Access Token for JIRA API Requests.
+//   JIRA_API_URL - JIRA API Url, it needs to be added in, or else requests won't work properly.
+//
+// Commands:
+//  hubot log <hours> on <task> - Logs hours on JIRA.
+//  hubot add-user <username> password <password> - Adds JIRA user to Android 18's database.
+
+
+'use strict';
+const crypto = require('crypto');
+
+module.exports = robot => {
+    function encryptText(text, secret) {
+        let cipher = crypto.createCipher('aes-256-ctr', secret);
+        let crypted = cipher.update(text, 'utf8', 'hex');
+        crypted += cipher.final('hex');
+        return crypted;
+    }
+    
+    function decryptText(text, secret) {
+        let decipher = crypto.createDecipher('aes-256-ctr', secret);
+        let dec = decipher.update(text, 'hex', 'utf8');
+        dec += decipher.final('utf8');
+        return dec;
+    }
+    
+    function logHours(config) {
+        const worklog = {
+            comment: config.comment,
+            timeSpent: config.time 
+        };
+        
+        const url = `${process.env['JIRA_API_URL']}/issue/${config.jiraNumber}/worklog`;
+        
+        return new Promise((resolve, reject) => {
+            robot.http(url, { rejectUnauthorized: false, muteHttpExceptions: false })
+            .header('Content-Type', 'application/json')
+            .header('Authorization', 'Basic ' + config.userpass)
+            .header('app_token', config.projectToken)
+            .post(JSON.stringify(worklog))((err, res, body) => {
+                if (res.statusCode === 400) {
+                    reject('I received a Bad Request from JIRA. :|');
+                }
+
+                if (res.status === 401) {
+                    reject('Your password is definitely wrong. Change it.');
+                } 
+                
+                resolve(`Yay! You logged ${config.time} on ${config.jiraNumber}`);
+            });
+        });
+    }
+    
+    robot.respond(/log (.+) on ([^\s]+)(.*)?/i, response => {
+        const hubotUserID = response.message.user.id;
+        let encryptedUserpass = robot.brain.get(hubotUserID);
+
+        if (!encryptedUserpass) {
+            response.send('You have ABSOLUTELY no credentials! Please add them with: add-user <username> password <password>');
+            return;
+        }
+        
+        let config = {
+            time: response.match[1],
+            jiraNumber: response.match[2],
+            comment: response.match[3],
+            projectToken: process.env['JIRA_PROJECT_TOKEN'],
+            userpass: decryptText(encryptedUserpass, hubotUserID)
+        };
+        
+        logHours(config)
+            .then(successMessage => response.send(successMessage))
+            .catch(error => response.send(error));
+    });
+    
+    robot.respond(/add-user (.+) password (.[^\s]+)/, response => {
+        const username = response.match[1];
+        const password = response.match[2];
+        // try getting unique trait from slack user, in this case: Slack ID
+        // encrypt based on Slack ID, robot should only be accessible through Slack
+        const hubotUserID = response.message.user.id;
+        
+        if (response.message.user.profile.email !== username) {
+            response.send('Stop trying to use other people\'s usernames, bro!');
+            return;
+        }
+        
+        try {
+            let userpassBase64 = new Buffer(username + ':' + password).toString('base64');
+            let encryptedUserpass = encryptText(userpassBase64, hubotUserID);
+            
+            robot.brain.set(hubotUserID, encryptedUserpass);
+            
+            response.send('Your user was successfully added to my database.');    
+        } catch (err) {
+            response.send(err);
+        }
+    });
+};
